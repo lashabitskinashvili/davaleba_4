842add115578f8fb35524798b873717f7d7d6e01
lasha <lasha.bitskinashvili.1@btu.edu.ge>
2021-12-26 15:47:02 +0400

    davaleba_4

---
+++ app/src/main/java/com/example/davaleba_4/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.davaleba_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var navigation = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
+        var controller = findNavController(R.id.nav_host_fragment_container)
+
+        var appBarConfiguration = AppBarConfiguration(
+            setOf(R.layout.fragment_blank, R.layout.fragment_blank2)
+        )
+
+       setupActionBarWithNavController(controller, appBarConfiguration)
+        navigation.setupWithNavController(controller)
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/davaleba_4/fragments/BlankFragment2.kt
@@ -0,0 +1,8 @@
+package com.example.davaleba_4.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.davaleba_4.R
+
+class BlankFragment2 : Fragment(R.layout.fragment_blank2) {
+
+}
